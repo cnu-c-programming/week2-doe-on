aaa02be0e4140579a4365a71f43dcb62faeaaedf
doe-on <fovusshi01197@gmail.com>
2026-03-13 11:36:35 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -3,9 +3,9 @@
 int main()
 {
     int var1 = 100;
-    printf("int : %d/n", var1);
-    printf("hex : %x/n", var1);
-    printf("oct : %o/n", var1);
+    printf("int : %d\n", var1);
+    printf("hex : %x\n", var1);
+    printf("oct : %o\n", var1);
 
     char var2 = 'c';
     printf("char : %c\n", var2);
