1c5857f8d4b29a080977a89de3a89ac88ac615e9
doe-on <fovusshi01197@gmail.com>
2026-03-18 21:27:37 +0900

    Update 3-2.c

--- code/3-2.c
+++ code/3-2.c
@@ -2,6 +2,17 @@
 
 int main()
 {
+    int main()
+{
+    int a = -1;
+    unsigned int b = 1;
+
+    if (a < b) {
+        printf("a < b\n");
+    }
+    else {
+        printf("a >= b ???\n");
+    }
 
     return 0;
 }
