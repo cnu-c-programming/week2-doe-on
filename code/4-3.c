227ecb8cb61e658c2572416abb4e293641c86567
doe-on <fovusshi01197@gmail.com>
2026-03-18 22:25:09 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -3,17 +3,20 @@
 int main()
 {
     int n;
-
     scanf("%d", &n);
 
+    if (n <= 1) {
+        printf("false");
+        return 0;
+    }
+
     for (int i = 2; i < n; i++) {
-        if ( n % i == 0 ){
-            printf("%s", "false");
+        if (n % i == 0) {
+            printf("false");
             return 0;
         }
     }
 
-     printf("true");
-     return 0;
+    printf("true");
+    return 0;
 }
-
